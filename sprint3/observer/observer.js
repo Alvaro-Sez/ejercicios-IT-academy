ca5fde4e4030e68ecc9ed5ca3e453dc8bc9d1213
Alvaro-Sez <87758728+Alvaro-Sez@users.noreply.github.com>
2021-09-13 19:15:05 +0200

    Update observer.js

--- sprint3/observer/observer.js
+++ sprint3/observer/observer.js
@@ -1,35 +1,32 @@
-/* - Exercici 1 - Observer
-1 - Creu una Aplicació que creï diferents objectes Usuari.
-2 - L'aplicació podrà crear diferents topics
-3 - els usuaris es poden subscriure.
-4- Quan un Usuari afegeixi un missatge, s'imprimirà per consola des del topic. També ho
-  imprimiran per consola cadascun dels usuaris que estiguin subscrits al topic (rebran el missatge). 
-5- Creu un topic amb un usuari i un altre amb dos, i mostri la recepció dels missatges pels 
-  usuaris. Utilitzi el modulo events. */
+/* he permitido que el programa envie el mensaje de un usuario a los usuarios que estan suscritos al mismo topico, incluyendo al propio usuario, es decir el usuario que envia el mensaje tambien recibira la alerta
+circular del topico anunciando que un nuevo mensaje ha sido recibido, segun el enunciado no entiendo que haya
+ningun problema en esto.. */
 
 const events = require('events'),
 emitter = new events.EventEmitter();
 
-let topics = [],
-usuaris = [],
-suscripcions = []
+let suscripcions = []
 
-emitter.on('newMsg', (name)=>console.log(name))
+emitter.on('newMsg', (name, topic, msg)=> comunicarMembresTopic(name,topic,msg))
 
 function Usuari (name){
   this.name = name
   this.enviarMissatge = function(msg){
-    console.log(msg)
-      emitter.emit('newMsg',name)
+    console.log(`\nEl ${name} envia un missatge que diu:\n${msg}\n`)
+    let topicSuscrit
+      suscripcions.forEach(el=>
+    { 
+      if(name == el.usuari.name )
+      {
+      topicSuscrit = el.topic.tema
+      }
+    })
+    emitter.emit('newMsg',name, topicSuscrit, msg)
   }
 }
-
+  
 function Topic (tema) {
     this.tema = tema
-
-    this.missatgeAlert = function(nom){
-      console.log(`nou missatge sobre: ${tema}, de part de l'usuari ${nom}`)
-    }
 }
 
 function Suscripcio (usuari,topic){
@@ -37,11 +34,22 @@ function Suscripcio (usuari,topic){
   this.topic = topic
 }
 
-  /* creacio usuaris */
+function comunicarMembresTopic(nom,topic,msg){
+  suscripcions.forEach(el => 
+  {
+    if(el.topic.tema === topic){
+      console.log(`CONSOLA de ${el.usuari.name}:\ns'ha penjat un nou missatge de l'usuari: ${nom}, sobre el topic: ${topic}\n el missatge diu: ${msg}\n`)
+    }   
+  })
+}
+
+
+
+
+    /* creacio usuaris */
 let Joan = new Usuari('Joan')
 let Pau = new Usuari('Pau')
 let Marta = new Usuari('Marta')
-
 
   /* creacio topics */
 let futbol = new Topic('futbol')
@@ -53,10 +61,8 @@ suscripcions.push(new Suscripcio(Joan, futbol))
 suscripcions.push(new Suscripcio(Pau, ciencia))
 suscripcions.push(new Suscripcio(Marta, ciencia))
 
-Joan.enviarMissatge('futbol de part de joan')
-futbol.missatgeAlert(Joan.name)
 
-
-
+Joan.enviarMissatge('parlem de futbol')
+Pau.enviarMissatge('Parlem de ciencia')
 
 
